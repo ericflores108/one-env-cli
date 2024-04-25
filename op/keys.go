6f7fa61b741758cf1c4c9c862b12cb597cd1a46a
Eric Flores <95382086+ericflores108@users.noreply.github.com>
2024-04-25 13:37:27 -0400

    refactor GetKeyConfig

---
+++ op/keys.go
@@ -0,0 +1,38 @@
+package op
+
+import (
+	"errors"
+
+	"github.com/spf13/viper"
+)
+
+type Key struct {
+	Vault      string
+	KeyName    string
+	SecretName string
+}
+
+func GetKeyConfig(integratedParty string) (Key, error) {
+	var key Key
+	if integratedParty == "" {
+		return key, errors.New("integratedParty is empty")
+	}
+
+	vault := viper.GetString("op.vault")
+	if vault == "" {
+		return key, errors.New("op.vault is empty")
+	}
+
+	viperStringPrefix := "env." + integratedParty
+	viperKeyName := viper.GetString(viperStringPrefix + ".keyName")
+	viperSecretName := viper.GetString(viperStringPrefix + ".keySecretName")
+
+	if viperKeyName == "" {
+		return key, errors.New("viperKeyName is empty")
+	}
+	if viperSecretName == "" {
+		return key, errors.New("viperKeyName is empty")
+	}
+
+	return Key{Vault: vault, KeyName: viperKeyName, SecretName: viperSecretName}, nil
+}
---
+++ op/keys_test.go
@@ -0,0 +1,22 @@
+package op
+
+import (
+	"testing"
+
+	"github.com/spf13/viper"
+)
+
+func TestGetKeyConfig(t *testing.T) {
+	viper.AddConfigPath("..")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	err := viper.ReadInConfig()
+	if err != nil {
+		t.Errorf("Expected error: %v", err)
+	}
+	key, err := GetKeyConfig("postman")
+	if err != nil {
+		t.Errorf("Expected error: %v", err)
+	}
+	t.Log(key)
+}
