4437069cf7f00885d3348ba1944bb60fe2f17bc9
Eric Flores <95382086+ericflores108@users.noreply.github.com>
2024-04-27 11:18:11 -0700

    feat(postman): transform op item to EnvironmentData

--- postman/utils.go
+++ postman/utils.go
@@ -1,5 +1,31 @@
 package postman
 
-func TransformItemToEnv() {
+import (
+	op "github.com/ericflores108/one-env-cli/op"
+)
 
+func TransformItemToEnv(item op.ItemResponse) EnvironmentData {
+	var envVars []EnvironmentVariable
+
+	for _, field := range item.Fields {
+		envType := DefaultType
+		if field.Type == "CONCEALED" {
+			envType = SecretType
+		}
+
+		envVar := EnvironmentVariable{
+			Key:     field.ID,
+			Value:   field.Value,
+			Enabled: true,
+			Type:    envType,
+		}
+		envVars = append(envVars, envVar)
+	}
+
+	envData := EnvironmentData{
+		Name:   item.Title,
+		Values: envVars,
+	}
+
+	return envData
 }
